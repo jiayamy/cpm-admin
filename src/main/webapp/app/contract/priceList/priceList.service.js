fe82feb4a7fb690e759d96dd12fbe5fd7c7790a7
sunhui <13627243658@163.com>
2017-01-10 17:25:03 +0800

    提交冲突

--- src/main/webapp/app/contract/priceList/priceList.service.js
+++ src/main/webapp/app/contract/priceList/priceList.service.js
@@ -8,14 +8,23 @@
     PriceListService.$inject = ['$resource'];
 
     function PriceListService ($resource) {
-        var service = $resource('contract/priceList', {}, {
+        var service = $resource('contract/priceList/:id', {}, {
             'query': {
                 method: 'GET',
                 isArray: true,
                 params: {name: null, type: null, source: null}
-            }
+            },
+            'get': {
+                method: 'GET',
+                transformResponse: function (data) {
+                    data = angular.fromJson(data);
+                    return data;
+                }
+            },
+            'save': { method:'POST' },
+            'update': { method:'PUT' },
+            'delete':{ method:'DELETE'}
         });
-
         return service;
     }
 })();
--- src/main/webapp/app/contract/priceList/priceList.state.js
+++ src/main/webapp/app/contract/priceList/priceList.state.js
@@ -11,7 +11,7 @@
         $stateProvider
         .state('priceList', {
             parent: 'contract',
-            url: '/priceList',
+            url: '/priceList?page&sort',
             data: {
                 authorities: ['ROLE_ADMIN'],
                 pageTitle: 'priceList.title'
@@ -22,17 +22,34 @@
                     controller: 'PriceListController',
                     controllerAs: 'vm'
                 }
+            },    params: {
+                page: {
+                    value: '1',
+                    squash: true
+                },
+                sort: {
+                    value: 'id,asc',
+                    squash: true
+                }
             },
             resolve: {
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                    $translatePartialLoader.addPart('priceList');
-                    return $translate.refresh();
-                }]
-            }
-        })
+            	 pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
+                     return {
+                         page: PaginationUtil.parsePage($stateParams.page),
+                         sort: $stateParams.sort,
+                         predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                         ascending: PaginationUtil.parseAscending($stateParams.sort)
+                     };
+                 }],
+                 translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                     $translatePartialLoader.addPart('priceList');
+                     return $translate.refresh();
+                 }]
+
+             }        })
         .state('priceList-detail',{
         	parent: 'contract',
-            url: '/priceList',
+            url: '/priceList/:id',
             data: {
                 authorities: ['ROLE_ADMIN'],
                 pageTitle: 'priceList.title'
@@ -40,7 +57,7 @@
             views: {
                 'content@': {
                     templateUrl: 'app/contract/priceList/priceList-detail.html',
-                    controller: 'PriceListController',
+                    controller: 'PriceListDetailController',
                     controllerAs: 'vm'
                 }
             },
@@ -51,5 +68,83 @@
                 }]
             }
         })
+        .state('priceList.edit', {
+            parent: 'contract',
+            url: '/{id}/edit',
+            data: {
+                authorities: ['ROLE_ADMIN']
+            },
+            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'app/contract/priceList/priceList-edit-dialog.html',
+                    controller: 'PriceListEditContoller',
+                    controllerAs: 'vm',
+                    backdrop: 'static',
+                    size: 'lg',
+                    resolve: {
+                        entity: ['PriceListService', function(PriceListService) {
+                            return PriceListService.get({id : $stateParams.id});
+                        }]
+                    }
+                }).result.then(function() {
+                    $state.go('priceList', null, { reload: true });
+                }, function() {
+//                    $state.go('^');
+                });
+            }]
+        })
+        .state('priceList.new', {
+            parent: 'contract',
+            url: '/new',
+            data: {
+                authorities: ['ROLE_ADMIN']
+            },
+            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'app/contract/priceList/priceList-edit-dialog.html',
+                    controller: 'PriceListEditContoller',
+                    controllerAs: 'vm',
+                    backdrop: 'static',
+                    size: 'lg',
+                    resolve: {
+                        entity: function () {
+                            return {
+                                id: null, name: null, type: null, unitPrice: null, unit: null,
+                                source: null, creator: null, creationTime: null, updater: null,
+                                updateTime: null
+                            };
+                        }
+                    }
+                }).result.then(function() {
+                    $state.go('priceList', null, { reload: true });
+                }, function() {
+                    $state.go('priceList');
+                });
+            }]
+        })
+        .state('priceList.delete', {
+            parent: 'contract',
+            url: '/{id}/delete',
+            data: {
+                authorities: ['ROLE_ADMIN']
+            },
+            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'app/contract/priceList/priceList-delete-dialog.html',
+                    controller: 'PriceListDeleteController',
+                    controllerAs: 'vm',
+                    size: 'md',
+                    resolve: {
+                        entity: ['PriceListService', function(PriceListService) {
+                            return PriceListService.get({id : $stateParams.id});
+                        }]
+                    }
+                }).result.then(function() {
+                    $state.go('priceList', null, { reload: true });
+                }, function() {
+//                    $state.go('^');
+                });
+            }]
+        });
     }
 })();
