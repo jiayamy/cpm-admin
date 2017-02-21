8df50d0ad0509c01948ccc680c3edd0a46053c17
lijia <464346304@qq.com>
2017-02-21 15:57:02 +0800

    修改model，修改定时任务

---
+++ src/main/java/com/wondertek/cpm/repository/ShareCostRateRepository.java
@@ -0,0 +1,9 @@
+package com.wondertek.cpm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wondertek.cpm.domain.ShareCostRate;
+
+public interface ShareCostRateRepository extends JpaRepository<ShareCostRate,Long>{
+
+}
