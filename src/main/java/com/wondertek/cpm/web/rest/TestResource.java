bed49ef05950ae56a421c3acc6a1b96f2b782683
lvliuzhong <lvliuzhong@wondertek.com.cn>
2017-03-01 17:49:44 +0800

    提交

--- src/main/java/com/wondertek/cpm/web/rest/TestResource.java
+++ src/main/java/com/wondertek/cpm/web/rest/TestResource.java
@@ -14,22 +14,18 @@ import com.wondertek.cpm.config.DateUtil;
 import com.wondertek.cpm.job.AccountScheduledJob;
 import com.wondertek.cpm.security.AuthoritiesConstants;
 
-import io.swagger.annotations.ApiParam;
-
 @RestController
 @RequestMapping("/api")
-public class testResource extends AccountScheduledJob{
+public class TestResource extends AccountScheduledJob{
 	
-	
-	@GetMapping("/test11")
+	@GetMapping("/test/")
 	@Timed
 	@Secured(AuthoritiesConstants.ADMIN)
-	public @ResponseBody String generateProjectWeeklyState(@ApiParam(value="date", required = false) @RequestParam(value="date", required = false) Long date) {
+	public @ResponseBody String generateProjectWeeklyState(@RequestParam(value="date", required = false) Long date) {
 		if(date == null){
 			//每周一晚上22点开始跑定时任务
 			accountScheduled(new Date());
 		}else{
-			
 			accountScheduled(DateUtil.parseDate("yyyyMMdd", date.toString()));
 		}
 		return "success";
