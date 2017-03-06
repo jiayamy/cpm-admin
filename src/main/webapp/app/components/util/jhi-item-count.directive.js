8ca748c06bbc0b995ec50cff156e186c1ca0dab6
lvliuzhong <lvliuzhong@wondertek.com.cn>
2017-03-06 09:54:33 +0800

    部分代码优化

--- src/main/webapp/app/components/util/jhi-item-count.directive.js
+++ src/main/webapp/app/components/util/jhi-item-count.directive.js
@@ -3,7 +3,7 @@
 
     var jhiItemCount = {
         template: '<div class="info">' +
-                    '当前 {{$ctrl.page ? ((($ctrl.page - 1) * $ctrl.itemsPerPage) == 0 ? 1 : (($ctrl.page - 1) * $ctrl.itemsPerPage + 1)) : 0}} - ' +
+                    '当前 {{$ctrl.page ? ($ctrl.queryCount == 0 ? 0 : ((($ctrl.page - 1) * $ctrl.itemsPerPage) == 0 ? 1 : (($ctrl.page - 1) * $ctrl.itemsPerPage + 1))) : 0}} - ' +
                     '{{$ctrl.page ? (($ctrl.page * $ctrl.itemsPerPage) < $ctrl.queryCount ? ($ctrl.page * $ctrl.itemsPerPage) : $ctrl.queryCount) : 0}} ' +
                     '总计 {{$ctrl.queryCount ? $ctrl.queryCount : 0}} 条' +
                 '</div>',
