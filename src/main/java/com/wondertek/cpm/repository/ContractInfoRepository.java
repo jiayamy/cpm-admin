eb027e94ae3946c1a1ec68ebd0fefed4944f7de7
zhangpengpeng <1152947695@qq.com>
2017-04-07 14:16:38 +0800

    update

--- src/main/java/com/wondertek/cpm/repository/ContractInfoRepository.java
+++ src/main/java/com/wondertek/cpm/repository/ContractInfoRepository.java
@@ -24,4 +24,7 @@ public interface ContractInfoRepository extends JpaRepository<ContractInfo,Long>
 	List<ContractInfo> findByStatusOrEndTime(Integer status, ZonedDateTime endTime);
 	
 	Optional<ContractInfo> findOneBySerialNum(String serialNum);
+	
+	@Query(" from ContractInfo where deptId is not null and (status = ?1 or updateTime >= ?2)")
+	List<ContractInfo> findByDeptIdAndStatusOrEndTime(Integer status, ZonedDateTime endTime);
 }
