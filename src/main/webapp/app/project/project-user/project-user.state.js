b36f9c5ae3a9d549482991dbe710541e6b2a8b34
zhangjun <dongyueershiyi0105@163.com>
2017-05-19 16:06:52 +0800

    员工勤奋度统计

--- src/main/webapp/app/project/project-user/project-user.state.js
+++ src/main/webapp/app/project/project-user/project-user.state.js
@@ -13,7 +13,7 @@
             parent: 'project',
             url: '/project-user?page&sort&projectId&userId&userName',
             data: {
-                authorities: ['ROLE_WORKHARDING'],
+                authorities: ['ROLE_PROJECT_USER'],
                 pageTitle: 'cpmApp.projectUser.home.title'
             },
             views: {
@@ -68,7 +68,7 @@
             parent: 'project-user',
             url: '/queryDept?selectType&showChild',
             data: {
-                authorities: ['ROLE_WORKHARDING']
+                authorities: ['ROLE_PROJECT_USER']
             },
             onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
                 $uibModal.open({
